5d5aab6fae5109f7d1ba6ecfa709a3159db16be3
Abdullah <abdullahqazi.77@gmail.com>
2022-01-17 00:49:13 +0530

    3. kth_min_max

--- Java/450Dsa/3. kth_max_min.java
+++ Java/450Dsa/3. kth_max_min.java
@@ -1,10 +1,46 @@
 import java.util.*;
 class kth_max_min {
+    static public void swap(int[] a, int i, int j){
+        int p=a[i];
+        a[i]=a[j];
+        a[j]=p;
+    }
+    public static int partition(int[] a, int l, int u){
+        int p=a[u], j=l;
+        for(int i=l;i<u;i++){
+            if(a[i]<p){
+                swap(a, i, j);
+                j++;
+            }
+        }
+        swap(a, j, u);
+        return j;
+    }
     static int kth_min(int[] a,int l, int u, int k){
-        return 0;
+        if(k<=u+1 && k>0){
+            int r=partition(a, l, u);
+            if(r+1==k){
+                return a[r];
+            }
+            else if(r+1>k){
+                return kth_min(a, l, r-1, k);
+            }
+            return kth_min(a, r+1, u, k);
+        }
+        return -1;
     }
     static int kth_max(int[] a,int l, int u, int k){
-        return 0;
+        if(k<=u+1 && k>0){
+            int r=partition(a, l, u);
+            if(r+1==k){
+                return a[r];
+            }
+            else if(r+1>k){
+                return kth_max(a, l, r-1, k);
+            }
+            return kth_max(a, r+1, u, k);
+        }
+        return -1;
     }
     public static void main(String args[])
     {
@@ -17,7 +53,7 @@ class kth_max_min {
         System.out.println("Enter k");
         int k=in.nextInt();
         System.out.println(kth_min(a, 0, 9, k));
-        System.out.println(kth_max(a, 0, 9, k));
+        System.out.println(kth_max(a, 0, 9, 10-k+1));
         in.close();
     }
 }
