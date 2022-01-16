8da55f46d5d7c1cec191330f3e9d6d4e5847dbff
Abdullah <abdullahqazi.77@gmail.com>
2022-01-16 23:23:30 +0530

    1. Reverse_array

--- Java/450Dsa/1. Reverse_array.java
+++ Java/450Dsa/1. Reverse_array.java
@@ -14,8 +14,9 @@ class Reverse_array{
             a[u--]=p;
         }
         System.out.println("Reversed elements are :");
-        for(int i=0;i<10;i++)
+        for(int i=0;i<10;i++){
             System.out.println(a[i]);
+        }
         in.close();
     }
 }
