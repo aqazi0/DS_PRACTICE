3510ecd0faa82193a6055e70ab0f66bfa2c76cbc
Abdullah <abdullahqazi.77@gmail.com>
2022-01-21 14:24:31 +0530

    17. best time to but and sell stock

--- 450Dsa/Java/17. Best Time to Buy and Sell Stock.java
+++ 450Dsa/Java/17. Best Time to Buy and Sell Stock.java
@@ -3,7 +3,6 @@ class Best_Time_to_Buy_and_Sell_Stock {
     public static int best_time(int[] a){
         int l=0, max=0, profit=0;
         for(int i=1;i<a.length;i++){
-            System.out.println(a[i]+"   "+l+"  ");
             if(a[i]<a[l]){
                 l=i;            
             }
