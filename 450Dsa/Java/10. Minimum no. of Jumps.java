4c4e71ec1ba89040f32b30e326c692a89fb8cbf7
Abdullah <abdullahqazi.77@gmail.com>
2022-01-24 02:55:33 +0530

    10. Minimum no of jumps

--- 450Dsa/Java/10. Minimum no. of Jumps.java
+++ 450Dsa/Java/10. Minimum no. of Jumps.java
@@ -34,25 +34,28 @@
 
 
 // Optimized Approach O(n)
-
+// leetcode 45
 
 import java.util.*;
 class Minimum_no_of_Jumps {
     public static int jumps(int[] a){
-        if(a.length==0)
+        if(a.length<=1)
             return 0;
-        int i=0, max=0, n=1, k=i+a[i];
-        while(k<a.length-1){
-            if(k==i)
-                return -1;
-            max=i+1;
-            for(int j=i+1;j<=k;j++){
-                if(j+a[j]>=max+a[max])
-                        max=j;
-                }
-            i=max;
-            k=i+a[i];
-            n++;
+        int i=0, max=a[0], n=0, k=a[0];
+        for(i=1;i<a.length;i++){
+            if(k+i-1>=a.length-1)
+            {
+                n++;
+                break;
+            }
+            max=Math.max(max, a[i]+i);
+            k--;
+            if(k==0){
+                n++;
+                if(i>=max)
+                    return -1;
+                k=max-i;
+            }
         }
         return n;
     }
