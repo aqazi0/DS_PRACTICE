f0e533b9eac06c31a7ec175183bca5e4af3c7862
Abdullah <abdullahqazi.77@gmail.com>
2022-02-03 03:33:44 +0530

    36. Median of two sorted arrays of different size

--- 450Dsa/Java/36. Median of 2 sorted arrays of different size.java
+++ 450Dsa/Java/36. Median of 2 sorted arrays of different size.java
@@ -1,39 +1,44 @@
 import java.util.*;
 class Median_of_two_sorted_arrays_of_different_size {
     static double getMedian(int a[], int b[], int n, int m){
-        int i=0, j=0, c=1, n1=-1, n2=-1, k=0;
+        int i=0, j=0, c=1, n1=Integer.MIN_VALUE, n2=Integer.MIN_VALUE, k=0, mid=(n+m)/2;
         if((n+m)%2!=0)
             k=1;
-        while(i<n && j<m && c<=n+1){
+        if(n==0 && k==1)
+            return b[mid];
+        if(m==0 && k==1)
+            return a[mid];
+        while(i<n && j<m && c<=mid+1){
             if(a[i]>=b[j]){
-                if(c==(n+m)/2)
-                    n2=b[j];
-                if(c==((n+m)/2+1))
+                if(c==mid)
                     n1=b[j];
+                if(c==(mid+1))
+                    n2=b[j];
                 j++;
             }
             else{
-                if(c==(n+m)/2)
-                    n2=a[i];
-                if(c==((n+m)/2+1))
+                if(c==mid)
                     n1=a[i];
+                if(c==(mid+1))
+                    n2=a[i];
                 i++;
             }
             c++;
         }
-        if(i==n && n2==-1)
-            n2=b[j];
-        if(i==n && n1==-1){
-            
-            n2=b[j];
+        if(i==n && n1==Integer.MIN_VALUE){
+            n1=b[mid-n-1];
+            n2=b[mid-n];
         }
-        if(j==n && n2==-1)
-            n2=a[i];
-        if(j==n && n1==-1){
-            n2=a[i];
+        else if(i==n && n2==Integer.MIN_VALUE)
+            n2=b[j];
+        if(j==m && n1==Integer.MIN_VALUE){
+            n1=a[mid-n-1];
+            n2=a[mid-n];
         }
+        else if(j==m && n2==Integer.MIN_VALUE)
+            n2=a[i];
         if(k==1)
-            return n1;
+            return n2;
         else
             return (n1+n2)/2.0;
     }
