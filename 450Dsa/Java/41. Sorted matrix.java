2858195d041ea685ae363b40048a4c64c1dbe44d
Abdullah <abdullahqazi.77@gmail.com>
2022-02-23 23:41:03 +0530

    Sorted Matrix

---
+++ 450Dsa/Java/41. Sorted matrix.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Sorted_Matrix{
+    public static int[][] sort(int N, int Mat[][]) {
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                ans.add(Mat[i][j]);
+            }
+        }
+        Collections.sort(ans);
+        int k=0;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                Mat[i][j]=ans.get(k++);
+            }
+        }
+        return Mat;
+    }
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter dimension of matrix");
+        System.out.print("n-");
+        int n=in.nextInt();
+            int[][] a = new int[n][n];
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                a[i][j]=in.nextInt();
+        }
+        sort(a.length, a);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                System.out.print(a[i][j]+" ");
+            System.out.println();
+        }
+        in.close();
+    }
+}
