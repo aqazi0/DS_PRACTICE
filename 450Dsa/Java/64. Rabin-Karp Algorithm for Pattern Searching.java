514ad30c4e28c8130cf4dc5765141199d278a545
Abdullah <abdullahqazi.77@gmail.com>
2022-04-03 14:36:58 +0530

    64. Rabin-Karp Algorithm for Pattern Searching

--- 450Dsa/Java/64. Rabin-Karp Algorithm for Pattern Searching.java
+++ 450Dsa/Java/64. Rabin-Karp Algorithm for Pattern Searching.java
@@ -1,22 +1,26 @@
 import java.util.*;
 class Rabin_Karp_Algorithm{
     public static int hash(String p){
-        int h=1, n=p.length();
-        for(int i=1;i<=n;i++){
-            h=h+(i*(int)Math.pow(10, n-i+1));
+        int h=0, n=p.length();
+        for(int i=0;i<n;i++){
+            h=h+p.charAt(i);
         }
         return h;
     } 
     public static boolean search(String s, String p){
-        int h=hash(p);
+        long h=hash(p);
         int n=s.length(), m=p.length();
+        long g;
+        g=hash(s.substring(0, m));
         for(int i=0;i<n-m+1;i++){
-            String s1=s.substring(i,i+p.length());
-            if(hash(s1)==h){
+            String s1=s.substring(i,i+m);
+            if(g==h){
                 if(s1.equals(p)){
                     return true;
                 }
             }
+            if(i!=n-m)
+            g=g-s.charAt(i)+s.charAt(i+m);
         }
         return false;
     }
